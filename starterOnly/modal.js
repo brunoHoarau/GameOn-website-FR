0c76fd5f2823d1ea954068eab2d7c3f5b2a4996e
Bruno974 <hoarau.bruno@gmail.com>
2021-05-26 20:16:03 +0200

    close modal functional

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// launch close modal event
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal ))
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "";
 }
 
 
