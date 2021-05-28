a9f0dd56983384991f77fbaa687fe3f2cd03b611
Bruno974 <hoarau.bruno@gmail.com>
2021-05-28 13:05:32 +0200

    error msg under general condition

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,9 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
+const checkbox1 = document.getElementById("checkbox1");
+const btnSubmit = document.getElementById("btn-submit")
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,3 +33,18 @@ function closeModal() {
 }
 
 
+// Control checkbox general condition + Msg error
+const form = document.getElementsByTagName('form')[0];
+form.addEventListener('click', () => {
+  if(!checkbox1.checked){
+    document.getElementById('error-checkbox1').innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions";
+    document.getElementById('error-checkbox1').classList.add('error-visible');
+  } else {
+    document.getElementById('error-checkbox1').innerHTML = "";
+  }
+  
+})
+
+// control city choose + msg errror
+const radio = document.querySelectorAll('input');
+console.log(radio);
